a3575ccb42349e8942c7f444b908d4cf46f50144
Antony Alkmim <antony-alkmim@hotmail.com>
2017-02-01 23:58:40 -0200

    add markdown interpreter for news

--- app/views/news/news.js
+++ app/views/news/news.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app.news', ['ngRoute'])
+angular.module('app.news', ['ngRoute','ng-showdown'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/news', {
@@ -11,6 +11,16 @@ angular.module('app.news', ['ngRoute'])
 
 .controller('NewsCtrl', ['$scope', function($scope) {
 
-    $scope.message = "Sputinik está sendo construído!"
+    $scope.message = "Sputinik está sendo construído!";
+
+    var content = "![News Image](http://s2.glbimg.com/UaEgn1Hynm-NUk6Sh44_JKgBJHM=/s.glbimg.com/og/rg/f/original/2016/09/02/brasil.jpg)\n"
+                + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                + "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.";
+    $scope.news = {
+        content : content
+    };
+
 
 }]);
