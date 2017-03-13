00fbe75f9e95756953f6b2d266ba248b08d9f075
Antony Alkmim <antony-alkmim@hotmail.com>
2017-03-13 17:12:46 -0300

    set no-cache on server

--- routes.js
+++ routes.js
@@ -7,6 +7,13 @@ moment.locale('pt_BR');
 
 module.exports = function Routes(app){
 
+    // No CACHE
+    app.use(function (req, res, next) {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        next()
+    });
 
     //Routes
     //==========================================
